7f69010277e6cca77a2a5370957dff4910804112
astha9900 <105634914+astha9900@users.noreply.github.com>
2023-09-19 00:10:09 +0530

    Time: 68 ms (33.54%), Space: 97.4 MB (12.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int x=0;x<nums.length;x++){
+            set.add(nums[x]);
+        }  
+        int nums2[]=new int[set.size()];
+        int x=0;
+        for(int num:set){
+            nums2[x]=num;
+            x++;
+        }
+        Arrays.sort(nums2);
         int n=nums.length;
-        int dp[]=new int [n];
-        Arrays.fill(dp,1);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
+        int m=nums2.length;
+        int dp[][]=new int[n+1][m+1];  
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<m+1;j++){
+                if(nums[i-1]==nums2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
                 }
             }
         }
-        int largest=0;
-        for(int i:dp){
-            largest=Math.max(largest,i);
-        }
-        return largest;
+        
+        return dp[n][m];
     }
 }
