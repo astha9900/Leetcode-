b95ed0d8ea6eed8e531106946d66b7405ef8bd21
astha9900 <105634914+astha9900@users.noreply.github.com>
2024-07-23 23:50:41 +0530

    Time: 13 ms (71.42%), Space: 48.7 MB (37.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,18 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-       
-       for(int i= 0; i < nums.length; i++){
-           if(map.containsKey(nums[i])){
-               int freq = map.get(nums[i]);
-               freq++;
-               map.put(nums[i],freq);
-           }
-           else{
-               map.put(nums[i],1);
-           }
+    int n=nums.length;
+    int res[]=new int[k];
+    HashMap<Integer,Integer> mp=new HashMap<>();
+       for(int i=0;i<n;i++){
+           mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
        }
-       int arr[] = new int[k];
-       int j = 0;
-       for(int i = 0 ; i< k ;i++){
-           int freq = 0;
-           int max = 0;
-           for(Map.Entry<Integer,Integer> val : map.entrySet()){
-               if(val.getValue() > freq){
-                   freq = val.getValue();
-                   max = val.getKey();
-               }
-           }
-            map.put(max,0);
-            arr[j] = max;
-            j++;
-       }
-       return arr;
-        
+    PriorityQueue<Integer>minHeap=new PriorityQueue<>((a,b)->mp.get(b)-mp.get(a));
+        for(int key : mp.keySet()){
+            minHeap.add(key);
+        }     
+        for(int i=0;i<k;i++){
+            res[i]=minHeap.poll();
+        }
+        return res;
     }
 }
