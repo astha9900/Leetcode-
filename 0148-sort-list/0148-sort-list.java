db3fc9989d40b56aecaa73f1f3dde72dac2ad79d
astha9900 <105634914+astha9900@users.noreply.github.com>
2024-09-08 21:59:31 +0530

    Time: 44 ms (5.19%), Space: 54.9 MB (94.6%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,58 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    private ListNode getMid(ListNode head){
-        ListNode slow=head;
-        ListNode fast=head.next;
-        while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-    private ListNode merge(ListNode head1,ListNode head2) {
-    	ListNode mergedLL=new ListNode(-1);
-    	ListNode temp=mergedLL;
-    	while(head1!=null&&head2!=null) {
-    		if(head1.val<=head2.val) {
-    		temp.next=head1;
-    		head1=head1.next;
-    		temp=temp.next;
-    	} else {
-    		temp.next=head2;
-    		head2=head2.next;
-    		temp=temp.next;
-    	}
-    	}
-    	while(head1!=null) {
-    		temp.next=head1;
-    		head1=head1.next;
-    		temp=temp.next;
-    	}
-    	while(head2!=null) {
-    		temp.next=head2;
-    		head2=head2.next;
-    		temp=temp.next;
-    	}
-    	return mergedLL.next;
-    }
     public ListNode sortList(ListNode head) {
-        if(head==null||head.next==null){
-            return head;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        ListNode temp=head;
+        int i=0;
+        while(head!=null){
+            pq.add(head.val);
+            i++;
+            head=head.next;
         }
-        ListNode mid=getMid(head);
-        ListNode rightHead=mid.next;
-        mid.next=null;
-        ListNode newleft=sortList(head);
-        ListNode newright=sortList(rightHead);
-        return merge(newleft,newright);
+        ListNode ans=new ListNode(-1);
+        ListNode result=ans;
+        while(!pq.isEmpty()){
+            ListNode p=new ListNode(pq.poll());
+            result.next=p;
+            result=result.next;
+        }
+        return ans.next;
+        
     }
 }
