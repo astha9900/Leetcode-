253cbb75f1392eb2a991e2c273140555a229dc80
astha9900 <105634914+astha9900@users.noreply.github.com>
2024-09-15 02:01:52 +0530

    Time: 4 ms (73.13%), Space: 44.2 MB (60.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        char a[]=s.toCharArray();
+        char b[]=t.toCharArray();
+        if(a.length!=b.length){
+            return false;
         }
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(map.get(ch)!=null){
-                if(map.get(ch)==1){
-                    map.remove(ch);
-                }
-                else{
-                    map.put(ch,map.get(ch)-1);
-                }
-            }
-            else{
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=b[i]){
                 return false;
             }
         }
-        return map.isEmpty();
+        return true;
+        
     }
 }
